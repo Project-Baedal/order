5d4777b415a72e5b87229753808a30735bec1f69
limjintaek <jintaek019@gmail.com>
2025-04-17 23:16:05 +0900

    [#1] add. 테스트 코드 기본 세팅 ( application-test.yml, IntegrationTest, PostgresCleaner 추가 )

---
+++ src/test/java/com/baedal/order/global/test/IntegrationTest.java
@@ -0,0 +1,34 @@
+package com.baedal.order.global.test;
+
+import com.baedal.order.global.cleaner.PostgresCleaner;
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+abstract public class IntegrationTest {
+
+  @LocalServerPort
+  protected int port;
+
+  protected String baseUrl = "http://localhost:";
+
+  @Autowired
+  protected TestRestTemplate restTemplate;
+
+  @Autowired
+  private PostgresCleaner dataCleaner;
+
+  @Autowired
+  protected FixtureMonkey fixtureMonkey;
+
+  @AfterEach
+  void tearDown() {
+    dataCleaner.clear();
+  }
+}
