eda07efe6a4d2a38c8498da9395cf58caeb8a161
limjintaek <jintaek019@gmail.com>
2025-04-18 16:29:29 +0900

    [#1] fix. StoreServiceClient name/url 오표기 수정

--- src/main/java/com/baedal/order/adapter/out/api/client/StoreServiceClient.java
+++ src/main/java/com/baedal/order/adapter/out/api/client/StoreServiceClient.java
@@ -4,7 +4,7 @@ import com.baedal.order.adapter.out.api.dto.GetStoreResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "order", url = "${servers.order.url}")
+@FeignClient(name = "store", url = "${servers.store.url}")
 public interface StoreServiceClient {
 
   @GetMapping("/getStore")
