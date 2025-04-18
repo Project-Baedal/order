0af937795c8038dd116a05ee589d1908d1ffd5bd
limjintaek <jintaek019@gmail.com>
2025-04-18 16:30:12 +0900

    [#1] fix. ProductApiMapper responseToDomain 매핑 오류 수정

--- src/main/java/com/baedal/order/adapter/out/api/mapper/ProductApiMapper.java
+++ src/main/java/com/baedal/order/adapter/out/api/mapper/ProductApiMapper.java
@@ -4,9 +4,14 @@ import com.baedal.order.adapter.out.api.dto.GetProductResponse;
 import com.baedal.order.domain.product.Product;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ProductApiMapper {
 
   List<Product> responseToDomain(List<GetProductResponse> response);
+
+  @Mapping(target = "name", source = "productName")
+  @Mapping(target = "price", source = "productPrice")
+  Product responseToDomain(GetProductResponse response);
 }
