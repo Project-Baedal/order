9ddc06cf6ff446de417253838a9df46bd5a19757
limjintaek <jintaek019@gmail.com>
2025-04-20 20:41:23 +0900

    [#1] add. paymentInfo @NotNull 어노테이션 추가

--- src/main/java/com/baedal/order/adapter/in/web/dto/request/PaymentInfo.java
+++ src/main/java/com/baedal/order/adapter/in/web/dto/request/PaymentInfo.java
@@ -1,6 +1,7 @@
 package com.baedal.order.adapter.in.web.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import lombok.Getter;
 public class PaymentInfo {
 
   @Schema(description = "결제 수단")
+  @NotNull
   private String paymentMethod;
 
   @Schema(description = "총 결제 금액")
