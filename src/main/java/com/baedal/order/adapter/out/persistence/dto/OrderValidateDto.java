cc50ab7e7db113b9b0187aa5485fdcd03d8d5342
limjintaek <jintaek019@gmail.com>
2025-04-26 22:36:28 +0900

    [#5] del. 미사용 어노테이션 삭제

--- src/main/java/com/baedal/order/adapter/out/persistence/dto/OrderValidateDto.java
+++ src/main/java/com/baedal/order/adapter/out/persistence/dto/OrderValidateDto.java
@@ -1,14 +1,10 @@
 package com.baedal.order.adapter.out.persistence.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class OrderValidateDto {
 
   private String domain;
